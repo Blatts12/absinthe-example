7d45033abf973fa3e8e74fe5cb801db38f4d59fb
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-01-15 08:06:39 +0100

    Add example with `publish/3`

--- lib/ex_abs_web/graphql/resolvers/auth/user_resolvers.ex
+++ lib/ex_abs_web/graphql/resolvers/auth/user_resolvers.ex
@@ -21,6 +21,10 @@ defmodule ExAbsWeb.GraphQl.Auth.UserResolvers do
   @spec create_user(map(), map()) :: {:ok, User.t()} | {:error, term()}
   def create_user(%{input: args}, _resolution) do
     with {:ok, %User{} = user} <- Auth.create_user(args) do
+      # This is how you publish a subscription event from a resolver
+      # Absinthe.Subscription.publish(ExAbsWeb.Endpoint, user, user_created: "user_created")
+      #                               ^endpoint          ^payload, ^subscription_name: "topic"
+
       {:ok, user}
     end
   end

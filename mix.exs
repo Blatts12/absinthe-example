80a5ebe3ac983409e263a52ef56786b87d8b1bd2
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-01-16 09:34:50 +0100

    Add absinthe_relay to deps

--- mix.exs
+++ mix.exs
@@ -54,6 +54,7 @@ defmodule ExAbs.MixProject do
       {:absinthe, "~> 1.7"},
       {:absinthe_plug, "~> 1.5"},
       {:absinthe_phoenix, "~> 2.0.2"},
+      {:absinthe_relay, "~> 1.5.0"},
       {:dataloader, "~> 2.0.0"},
       {:paginator, git: "https://github.com/omarelb/paginator.git", branch: "loosen_plug_crypto_dependency_constraint"},
       {:ex_machina, "~> 2.7.0", only: :test},

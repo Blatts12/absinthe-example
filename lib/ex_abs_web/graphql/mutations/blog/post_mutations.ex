8b038c7e6160d7d117d5659b2d9e863a5c5d2265
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-01-17 13:28:27 +0100

    Use current_user in create post mutation

--- lib/ex_abs_web/graphql/mutations/blog/post_mutations.ex
+++ lib/ex_abs_web/graphql/mutations/blog/post_mutations.ex
@@ -9,14 +9,11 @@ defmodule ExAbsWeb.GraphQl.Blog.PostMutations do
     payload field :create_post do
       input do
         field :title, non_null(:string)
-        field :user_id, non_null(:id)
       end
 
       output do
         field :post, non_null(:post)
       end
-
-      middleware ParseIDs, input: [user_id: :user]
 
       resolve &PostResolvers.create_post/2
     end
--- lib/ex_abs_web/graphql/resolvers/blog/post_resolvers.ex
+++ lib/ex_abs_web/graphql/resolvers/blog/post_resolvers.ex
@@ -5,9 +5,16 @@ defmodule ExAbsWeb.GraphQl.Blog.PostResolvers do
   alias ExAbs.Blog.Post
 
   @spec create_post(map(), map()) :: {:ok, Post.t()} | {:error, term()}
-  def create_post(%{input: args}, _resolution) do
+  def create_post(args, %{context: %{current_user: %{id: user_id}}}) do
+    args = Map.put(args, :user_id, user_id)
+    IO.inspect(args)
+
     with {:ok, %Post{} = post} <- Blog.create_post(args) do
-      {:ok, post}
+      {:ok, %{post: post}}
     end
+  end
+
+  def create_post(_, _) do
+    {:error, :unauthorized}
   end
 end

000b18b4aae37f48c5a934f37ffad9e189e40244
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-01-18 09:41:49 +0100

    Remove more of old code

--- priv/repo/migrations/20240117063925_add_avatar_field_to_auth_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule ExAbs.Repo.Migrations.AddAvatarFieldToAuthUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:auth_users) do
-      add :avatar, :string, null: true
-    end
-  end
-end

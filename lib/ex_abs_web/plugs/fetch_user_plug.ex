0e4762ba788f774267f891ed0d588b21e384dae2
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-01-18 08:36:36 +0100

    Update fetch user plug to use token

--- lib/ex_abs_web/plugs/fetch_user_plug.ex
+++ lib/ex_abs_web/plugs/fetch_user_plug.ex
@@ -5,16 +5,16 @@ defmodule ExAbsWeb.FetchUserPlug do
 
   import Plug.Conn
 
-  alias ExAbs.Auth
-  alias ExAbs.Auth.User
+  alias ExAbs.Accounts
+  alias ExAbs.Accounts.User
 
   @spec init(Keyword.t()) :: Keyword.t()
   def init(opts), do: opts
 
   @spec call(Plug.Conn.t(), Keyword.t()) :: Plug.Conn.t()
   def call(conn, _opts) do
-    with ["Bearer " <> user_id] <- get_req_header(conn, "authorization"),
-         %User{} = user <- Auth.get_user(user_id) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+         %User{} = user <- Accounts.get_user_by_session_token(token) do
       Absinthe.Plug.assign_context(conn, :current_user, user)
     else
       _user_id_missing_or_incorrect -> conn

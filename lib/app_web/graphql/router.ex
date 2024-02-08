c5b6e41ef48dc7ef6e61c24e9b58b69ff67dba01
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 07:01:50 +0100

    About GraphiQL

--- lib/app_web/graphql/router.ex
+++ lib/app_web/graphql/router.ex
@@ -7,4 +7,12 @@ defmodule AppWeb.GraphQl.Router do
   forward "/graphql",
     to: Absinthe.Plug,
     init_opts: []
+
+  if Application.compile_env(:app, :dev_routes) do
+    forward "/graphiql",
+      to: Absinthe.Plug.GraphiQL,
+      init_opts: [
+        interface: :playground
+      ]
+  end
 end

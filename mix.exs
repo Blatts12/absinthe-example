b650a3b28d045a91ca6c0c0bb54dbc77888de2e4
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 06:58:16 +0100

    Install dependencies

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule App.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 

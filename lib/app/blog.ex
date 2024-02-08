bd5db945afceb252b9e2c5a19fad3e55025c2a1f
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 07:12:57 +0100

    Create the first query

--- lib/app/blog.ex
+++ lib/app/blog.ex
@@ -38,6 +38,22 @@ defmodule App.Blog do
   def get_post!(id), do: Repo.get!(Post, id)
 
   @doc """
+  Gets a single post.
+
+  Returns `nil` if the Post does not exist.
+
+  ## Examples
+
+      iex> get_post(123)
+      %Post{}
+
+      iex> get_post(456)
+      nil
+
+  """
+  def get_post(id), do: Repo.get(Post, id)
+
+  @doc """
   Creates a post.
 
   ## Examples
--- lib/app_web/graphql/schema.ex
+++ lib/app_web/graphql/schema.ex
@@ -12,8 +12,13 @@ defmodule AppWeb.GraphQl.Schema do
     field :updated_at, non_null(:datetime)
   end
 
-  # query do
-  # end
+  query do
+    field :get_post, :post do
+      arg :id, non_null(:id)
+
+      resolve fn %{id: id}, _ -> {:ok, App.Blog.get_post(id)} end
+    end
+  end
 
   # mutation do
   # end

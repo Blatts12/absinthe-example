342f49940184a5a152997fc81edfe1d973dfcb26
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 08:47:02 +0100

    Using middlewares globally

--- lib/app_web/graphql/schema.ex
+++ lib/app_web/graphql/schema.ex
@@ -1,12 +1,22 @@
 defmodule AppWeb.GraphQl.Schema do
   use Absinthe.Schema
 
+  alias AppWeb.GraphQl.Middleware.HandleChangesetErrors
+
   # datetime, native_datetime, decimal
   import_types Absinthe.Type.Custom
 
   import_types AppWeb.GraphQl.Mutations
   import_types AppWeb.GraphQl.Queries
   import_types AppWeb.GraphQl.Types
+
+  def middleware(middleware, _field, %{identifier: :mutation}) do
+    middleware ++ [HandleChangesetErrors]
+  end
+
+  def middleware(middleware, _field, _object) do
+    middleware
+  end
 
   query do
     # Accounts

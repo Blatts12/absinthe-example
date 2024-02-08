053a3170df7242c5f0c0ea29c6e287798944a75c
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 12:00:32 +0100

    Fetch a user in the user socket

--- lib/app_web/channels/user_socket.ex
+++ lib/app_web/channels/user_socket.ex
@@ -4,53 +4,32 @@ defmodule AppWeb.UserSocket do
   use Absinthe.Phoenix.Socket,
     schema: AppWeb.GraphQl.Schema
 
-  # A Socket handler
-  #
-  # It's possible to control the websocket connection and
-  # assign values that can be accessed by your channel topics.
+  alias App.Accounts
+  alias App.Accounts.User
 
-  ## Channels
-  # Uncomment the following line to define a "room:*" topic
-  # pointing to the `AppWeb.RoomChannel`:
-  #
-  # channel "room:*", AppWeb.RoomChannel
-  #
-  # To create a channel file, use the mix task:
-  #
-  #     mix phx.gen.channel Room
-  #
-  # See the [`Channels guide`](https://hexdocs.pm/phoenix/channels.html)
-  # for further details.
-
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error` or `{:error, term}`. To control the
-  # response the client receives in that case, [define an error handler in the
-  # websocket
-  # configuration](https://hexdocs.pm/phoenix/Phoenix.Endpoint.html#socket/3-websocket-configuration).
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
   @impl true
-  def connect(_params, socket, _connect_info) do
+  def connect(params, socket, _connect_info) do
+    params = Map.new(params, fn {k, v} -> {String.downcase(k), v} end)
+
+    socket =
+      if current_user = current_user(params) do
+        socket
+        |> assign(:current_user, current_user)
+        |> Absinthe.Phoenix.Socket.put_options(context: %{current_user: current_user})
+      end
+
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     Elixir.AppWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
+  defp current_user(%{"authorization" => "Bearer " <> token}) do
+    with %User{} = user <- Accounts.get_user(token) do
+      user
+    end
+  end
+
+  defp current_user(_), do: nil
+
   @impl true
+  def id(%Phoenix.Socket{assigns: %{current_user: %{id: id}}}), do: "user:#{id}"
   def id(_socket), do: nil
 end

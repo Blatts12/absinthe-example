e64c7a51bb21031289986bbd4251e3b2b9f5873d
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 06:58:44 +0100

    Configure the formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix],
+  import_deps: [:ecto, :ecto_sql, :phoenix, :absinthe],
   subdirectories: ["priv/*/migrations"],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"]
 ]

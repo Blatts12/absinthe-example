91cfb61643f94a2a1767317f2b55e69232b3b93b
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-02-08 08:45:28 +0100

    Using middleware per operation

--- lib/app_web/graphql/mutations/blog/post_mutations.ex
+++ lib/app_web/graphql/mutations/blog/post_mutations.ex
@@ -2,12 +2,14 @@ defmodule AppWeb.GraphQl.Blog.PostMutations do
   use Absinthe.Schema.Notation
 
   alias AppWeb.GraphQl.Blog.PostResolvers
+  alias AppWeb.GraphQl.Middleware.HandleChangesetErrors
 
   object :post_mutations do
     field :create_post, non_null(:post) do
       arg :input, non_null(:create_post_input)
 
       resolve &PostResolvers.create_post/2
+      middleware HandleChangesetErrors
     end
   end
 end
